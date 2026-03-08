f3fde1f465ca4f1a2e611320d5f428f90f348754
sandhya10-tech <sandhyabaghel104@gmail.com>
2026-03-08 11:59:00 +0530

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -4,35 +4,35 @@ st.write
 Score= 0
 st.write("Welcome to quiz game....")
 st.write('Q1. What is the first alphabetb of english language?\nA)C B)T\n C)A  D)F')
-ans = text_input("Enter your choice....")
+ans = st.text_input("Enter your choice....")
 if ans.lower()== 'c':
     Score +=5
 st.write('___________________________________________________________________________')
 
 st.write('Q2. What is the National Bird?\n A)Peacock  B)Crow\n  C)pegion  D)Duck')
-ans = text_input ("Enter Your Choice....")
+ans = st.text_input ("Enter Your Choice....")
 
 if ans.lower() == 'a':
     Score +=5
 st.write('----------------------------------------------------------------------------------')
 st.write("Q3. what is the national Animal?\n A) tiger B) lion  C) Elephant D) bear")
-ans= text_input("enter ur chioce....")
+ans= st.text_input("enter ur chioce....")
 if ans.lower() == 'a':
      Score +=5
 st.write('----------------------------------------------------------------------------')
 st.write('Q4. what is middle alphabet in CAT?\n A) T   B) C   C)W    D)A')
-ans = text_input("Enter ur choice....")
+ans = st.text_input("Enter ur choice....")
 if ans.lower() == 'd':
    Score+=5
 st.write('--------------------------------------------------------------------------')
 st.write ('What is the national vegetable?\n A)tomato B) onion  C) peas  D) brinjal')
-ans = text_input("Enter ur choice.....")
+ans = st.text_input("Enter ur choice.....")
 if ans.lower() == 'd':
      Score+=5
 st.write ('---------------------------------------------------------------------')
 if Score == 25:
   st.write("Congratulation !! you have scored__",Score, "1st position")
-  st.ballons()
+  st.balloons()
 if Score == 15:
   st.write("Congratulation !! you have scored__",Score,"2nd position")
   st.snow()
