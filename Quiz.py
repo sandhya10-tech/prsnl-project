35fc8cb2149185bc993a4e720354001a5966c2b2
sandhya10-tech <sandhyabaghel104@gmail.com>
2026-03-14 17:13:10 +0530

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -11,34 +11,34 @@ ans1 = st.text_input("Enter answer for_")
 
 # Q2
 st.write("Q2 What is the National Bird?\n A)Peacock  B)Crow\n  C)pegion  D)Duck")
-ans2 = st.text_input("Enter answer for_")
+ans = st.text_input("Enter answer for_")
 
 # Q3
 st.write("Q3. what is the national Animal?\n A) tiger B) lion  C) Elephant D) bear")
-ans3 = st.text_input("Enter answer for_")
+ans = st.text_input("Enter answer for_")
 
 # Q4
 st.write("Q4. what is middle alphabet in CAT?\n A) T   B) C   C)W    D)A")
-ans4 = st.text_input("Enter answer for_")
+ans = st.text_input("Enter answer for_")
 
 # Q5
 st.write("Q5.What is the national vegetable?\n A)tomato B) onion  C) peas  D) brinjal")
-ans5 = st.text_input("Enter answer for_")
+ans = st.text_input("Enter answer for_")
 
 # Submit button
 if st.button("Submit Quiz"):
 
     score = 0
 
-    if ans1.lower() == "a":
+    if ans.lower() == "a":
         score += 5
-    if ans2.lower() == "a":
+    if ans.lower() == "a":
         score += 5
-    if ans3.lower() == "a":
+    if ans.lower() == "a":
         score += 5
-    if ans4.lower() == "d":
+    if ans.lower() == "d":
         score += 5
-    if ans5.lower() == "d":
+    if ans.lower() == "d":
         score += 5
 
     if score == 25:
