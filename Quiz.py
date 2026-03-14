1f76098e3a61fadba2470b58307f082a4ed6ac85
sandhya10-tech <sandhyabaghel104@gmail.com>
2026-03-14 17:03:49 +0530

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -5,24 +5,25 @@ score = 0
 st.write("Welcome to quiz game....")
 
 # Q1
-st.write("Q1. What is the first alphabet of English language?")
-ans1 = st.text_input("Enter answer for Q1 (A/B/C/D)", key="q1")
+st.write("'Q1. What is the first alphabetb of english language?\nA)C B)T\n C)A  D)F'")
+    
+ans1 = st.text_input("Enter answer for_)
 
 # Q2
-st.write("Q2. What is the National Bird?")
-ans2 = st.text_input("Enter answer for Q2 (A/B/C/D)", key="q2")
+st.write(" What is the National Bird?\n A)Peacock  B)Crow\n  C)pegion  D)Duck'")
+ans2 = st.text_input("Enter answer for_")
 
 # Q3
-st.write("Q3. What is the National Animal?")
-ans3 = st.text_input("Enter answer for Q3 (A/B/C/D)", key="q3")
+st.write("Q3. what is the national Animal?\n A) tiger B) lion  C) Elephant D) bear")
+ans3 = st.text_input("Enter answer for_)
 
 # Q4
-st.write("Q4. What is middle alphabet in CAT?")
-ans4 = st.text_input("Enter answer for Q4 (A/B/C/D)", key="q4")
+st.write("Q4. what is middle alphabet in CAT?\n A) T   B) C   C)W    D)A")
+ans4 = st.text_input("Enter answer for_)
 
 # Q5
-st.write("Q5. What is the national vegetable?")
-ans5 = st.text_input("Enter answer for Q5 (A/B/C/D)", key="q5")
+st.write("Q5.What is the national vegetable?\n A)tomato B) onion  C) peas  D) brinjal")
+ans5 = st.text_input("Enter answer for_)
 
 # Submit button
 if st.button("Submit Quiz"):
